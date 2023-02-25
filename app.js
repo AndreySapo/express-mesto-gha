a7d053cd66dc958c4b9903445e357d0714d9916f
Andrey Sapo <andreytapok@yandex.ru>
2023-02-25 14:45:07 +0800

    Создал контроллеры и роуты для пользователей
    Проверено с помощью тестового пользователя

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const express = require('express');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const mongoose = require('mongoose');
@@ -9,5 +10,35 @@ mongoose.set('strictQuery', false);
 mongoose.connect('mongodb://0.0.0.0:27017/mestodb', {
   useNewUrlParser: true,
 });
+
+const User = require('./models/user');
+// const Card = require('./models/card');
+
+function getUsers(req, res) {
+  User.find({})
+    .then((users) => res.send({ data: users }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+}
+
+function getUserByID(req, res) {
+  User.findById(req.params.userId)
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+}
+
+function createUser(req, res) {
+  const { name, about, avatar } = req.query;
+
+  // eslint-disable-next-line no-undef, no-restricted-globals
+  User.create({ name, about, avatar })
+    .then((user) => res.send({ data: user }))
+    .catch((err) => res.status(500).send({ message: 'Произошла ошибка' }));
+}
+
+app.get('/users', getUsers);
+
+app.get('/users/:userId', getUserByID);
+
+app.post('/users', createUser);
 
 app.listen(3000);
