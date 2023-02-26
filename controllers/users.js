74a5c2235e7615196fab4eb2a5bc3dfaf264eb1a
Andrey Sapo <andreytapok@yandex.ru>
2023-02-26 14:20:01 +0800

    добавлена обработка ошибок user+отображение ответа

--- controllers/users.js
+++ controllers/users.js
@@ -1,26 +1,68 @@
 const User = require('../models/user');
+const { ERROR_BAD_REQUEST, ERROR_NOT_FOUND, ERROR_INTERNAL_SERVER } = require('../errors/errors');
 
+// получить всех юзеров
 module.exports.getUsers = (req, res) => {
   User.find({})
     .then((users) => res.send({ data: users }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch((err) => {
+      if (err.name === 'InternalServerError') {
+        res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
+        return;
+      }
+      res.send({ message: `Произошла неизвестная ошибка ${err.name}: ${err.message}` });
+    });
 };
 
+// получить юзера по айди
 module.exports.getUserByID = (req, res) => {
   User.findById(req.params.userId)
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .then((user) => {
+      if (!user) {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Пользователь по указанному _id не найден.' });
+        return;
+      }
+      res.send({
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+        _id: user._id,
+      });
+    })
+    .catch((err) => {
+      if (err.name === 'InternalServerError') {
+        res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
+        return;
+      }
+      res.send({ message: `Произошла неизвестная ошибка ${err.name}: ${err.message}` });
+    });
 };
 
+// создать юзера
 module.exports.createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
-  // eslint-disable-next-line no-undef, no-restricted-globals
   User.create({ name, about, avatar })
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .then((user) => res.send({
+      name: user.name,
+      about: user.about,
+      avatar: user.avatar,
+      _id: user._id,
+    }))
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+        return;
+      }
+      if (err.name === 'InternalServerError') {
+        res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
+        return;
+      }
+      res.send({ message: `Произошла неизвестная ошибка ${err.name}: ${err.message}` });
+    });
 };
 
+// обновить информацию по юзеру
 module.exports.updateUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
@@ -29,10 +71,32 @@ module.exports.updateUser = (req, res) => {
     { name, about, avatar },
     { new: true, runValidators: true, upsert: true },
   )
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .then((user) => {
+      if (!user) {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Пользователь по указанному _id не найден.' });
+        return;
+      }
+      res.send({
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+        _id: user._id,
+      });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при обновлении профиля.' });
+        return;
+      }
+      if (err.name === 'InternalServerError') {
+        res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
+        return;
+      }
+      res.send({ message: `Произошла неизвестная ошибка ${err.name}: ${err.message}` });
+    });
 };
 
+// обновить отдельно аватар юзера
 module.exports.updateAvatar = (req, res) => {
   const { avatar } = req.body;
 
@@ -41,6 +105,27 @@ module.exports.updateAvatar = (req, res) => {
     { avatar },
     { new: true, runValidators: true, upsert: true },
   )
-    .then((user) => res.send({ data: user }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .then((user) => {
+      if (!user) {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Пользователь по указанному _id не найден.' });
+        return;
+      }
+      res.send({
+        name: user.name,
+        about: user.about,
+        avatar: user.avatar,
+        _id: user._id,
+      });
+    })
+    .catch((err) => {
+      if (err.name === 'ValidationError') {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при обновлении аватара.' });
+        return;
+      }
+      if (err.name === 'InternalServerError') {
+        res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
+        return;
+      }
+      res.send({ message: `Произошла неизвестная ошибка ${err.name}: ${err.message}` });
+    });
 };
