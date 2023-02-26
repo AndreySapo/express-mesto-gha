29816bcf61616f4c8792f372b4aa5b4eacf12797
Andrey Sapo <andreytapok@yandex.ru>
2023-02-26 16:19:32 +0800

    добавлена обработка ошибок cards

--- controllers/cards.js
+++ controllers/cards.js
@@ -1,10 +1,16 @@
-/* eslint-disable no-unused-vars */
 const Card = require('../models/card');
+const { ERROR_BAD_REQUEST, ERROR_NOT_FOUND, ERROR_INTERNAL_SERVER } = require('../errors/errors');
 
 module.exports.getCards = (req, res) => {
   Card.find({})
     .then((cards) => res.send({ data: cards }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch((err) => {
+      if (err.name === 'InternalServerError') {
+        res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
+        return;
+      }
+      res.send({ message: `Произошла неизвестная ошибка ${err.name}: ${err.message}` });
+    });
 };
 
 module.exports.createCard = (req, res) => {
@@ -12,14 +18,38 @@ module.exports.createCard = (req, res) => {
   const owner = req.user._id;
 
   Card.create({ name, link, owner })
-    .then((card) => res.send({ data: card }))
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .then((card) => {
+      if (!card) {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные при создании карточки.' });
+        return;
+      }
+      res.send({ data: card });
+    })
+    .catch((err) => {
+      if (err.name === 'InternalServerError') {
+        res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
+        return;
+      }
+      res.send({ message: `Произошла неизвестная ошибка ${err.name}: ${err.message}` });
+    });
 };
 
 module.exports.deleteCard = (req, res) => {
   Card.findByIdAndRemove(req.params.cardId)
-    .then(() => { res.send(`Карточка ${req.params.cardId} удалена`); })
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .then((deletedCard) => {
+      if (!deletedCard) {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Карточка с указанным _id не найдена.' });
+        return;
+      }
+      res.send(`Карточка ${req.params.cardId} удалена`);
+    })
+    .catch((err) => {
+      if (err.name === 'InternalServerError') {
+        res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
+        return;
+      }
+      res.send({ message: `Произошла неизвестная ошибка ${err.name}: ${err.message}` });
+    });
 };
 
 module.exports.likeCard = (req, res) => {
@@ -29,9 +59,23 @@ module.exports.likeCard = (req, res) => {
     { new: true },
   )
     .then((card) => {
+      if (!card) {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные для постановки лайка' });
+        return;
+      }
       res.send({ data: card });
     })
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Передан несуществующий _id карточки' });
+        return;
+      }
+      if (err.name === 'InternalServerError') {
+        res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
+        return;
+      }
+      res.send({ message: `Произошла неизвестная ошибка ${err.name}: ${err.message}` });
+    });
 };
 
 module.exports.dislikeCard = (req, res) => {
@@ -41,7 +85,21 @@ module.exports.dislikeCard = (req, res) => {
     { new: true },
   )
     .then((card) => {
+      if (!card) {
+        res.status(ERROR_BAD_REQUEST).send({ message: 'Переданы некорректные данные для снятия лайка' });
+        return;
+      }
       res.send({ data: card });
     })
-    .catch((err) => res.status(500).send({ message: `Произошла ошибка ${err}` }));
+    .catch((err) => {
+      if (err.name === 'CastError') {
+        res.status(ERROR_NOT_FOUND).send({ message: 'Передан несуществующий _id карточки' });
+        return;
+      }
+      if (err.name === 'InternalServerError') {
+        res.status(ERROR_INTERNAL_SERVER).send({ message: 'Ошибка по умолчанию' });
+        return;
+      }
+      res.send({ message: `Произошла неизвестная ошибка ${err.name}: ${err.message}` });
+    });
 };
