1a7392ae575ed721e2520e6c139d2144d04b8dca
Andrey Sapo <andreytapok@yandex.ru>
2023-02-26 16:53:15 +0800

    исправил схемы

--- models/user.js
+++ models/user.js
@@ -5,14 +5,14 @@ const userSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Обязано быть больше 2 символов'],
+    maxlength: [30, 'Обязано быть меньше 30 символов'],
   },
   about: {
     type: String,
     required: true,
-    minlength: 2,
-    maxlength: 30,
+    minlength: [2, 'Обязано быть больше 2 символов'],
+    maxlength: [30, 'Обязано быть меньше 30 символов'],
   },
   avatar: {
     type: String,
