df2c8d402952d9821a18e27e8ea27593f539efc1
Andrey Sapo <andreytapok@yandex.ru>
2023-02-26 14:18:55 +0800

    почистил мелочи

--- app.js
+++ app.js
@@ -11,15 +11,13 @@ const app = express();
 
 mongoose.set('strictQuery', false);
 // подключаемся к серверу mongo
-mongoose.connect('mongodb://0.0.0.0:27017/mestodb', {
-  useNewUrlParser: true,
-});
+mongoose.connect('mongodb://0.0.0.0:27017/mestodb', { useNewUrlParser: true });
 
-app.use(bodyParser.json()); // для собирания JSON-формата
-app.use(bodyParser.urlencoded({ extended: true })); // для приёма веб-страниц внутри POST-запроса
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use((req, res, next) => {
   req.user = {
-    _id: '63f9f2e0156729d293d8f458',
+    _id: '63fadda2d6c0fd5e151a2963',
   };
 
   next();
